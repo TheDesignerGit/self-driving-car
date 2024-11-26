3e18da9a7363b6cb013e0b13ae332ccd5295cf69
JayJay Abrahams <jayson_lastong@yahoo.co.za>
2024-11-26 04:40:36 +0200

    added traffic with interactions

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-    constructor(x,y,w,h){
+    constructor(x,y,w,h, controlType, maxSpeed=3){
         this.x = x
         this.y = y
         this.w = w
@@ -7,28 +7,38 @@ class Car {
 
         this.speed = 0
         this.acceleration = 0.2
-        this.maxSpeed = 3
+        this.maxSpeed = maxSpeed
         this.friction = 0.05
         this.angle = 0
         this.damaged = false
 
         // this.polygon = null             // 
 
-        this.sensor = new Sensor(this)          // pass the car object into the sensor
-        this.controls = new Controls()
+        if(controlType != "DUMMY"){
+            this.sensor = new Sensor(this)          // pass the car object into the sensor
+        }
+        this.controls = new Controls(controlType)
     }
 
-    update(roadBorders){
-        this.#move()
-        this.polygon = this.#createPolygon()
-        this.damaged = this.#assessDamage(roadBorders)
-        this.sensor.update(roadBorders)
+    update(roadBorders, traffic){
+        if(!this.damaged){
+            this.#move()
+            this.polygon = this.#createPolygon()
+            this.damaged = this.#assessDamage(roadBorders, traffic)
+        }
+        if(this.sensor){
+            this.sensor.update(roadBorders, traffic)
+        }
     }
 
-    #assessDamage(roadBorders) {
+    #assessDamage(roadBorders, traffic) {
         for(let i=0; i<roadBorders.length; i++) {
             // is there an intersection between the car and the border
             if(polysIntersect(this.polygon, roadBorders[i])) {return true}
+        }
+        for(let i=0; i<traffic.length; i++) {
+            // is there an intersection between the car and the border
+            if(polysIntersect(this.polygon, traffic[i].polygon)) {return true}
         }
         return false
     }
@@ -110,6 +120,8 @@ class Car {
         }
         ctx.fill()
 
-        this.sensor.draw(ctx)           // draw the sensors
+        if(this.sensor){
+            this.sensor.draw(ctx)           // draw the sensors
+        }
     }
 }
--- main.js
+++ main.js
@@ -3,18 +3,27 @@ canvas.width = 200
 
 const ctx = canvas.getContext('2d')             // get access to canvas methods
 const road = new Road(canvas.width/2, canvas.width*0.9)
-const car = new Car(road.getLaneCenter(1),100,30,50)        // arg-1 = which lane to start on
-
+const car = new Car(road.getLaneCenter(1),100,30,50, "KEYS")        // arg-1 = which lane to start on
+const traffic = [
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+]
 
 animate()
 
 function animate () {
-    car.update(road.borders)
+    for(let i=0; i<traffic.length; i++){
+        traffic[i].update(road.borders, [])             // update the traffic, keeping in mind the borders
+    }
+    car.update(road.borders, traffic)
     canvas.height = window.innerHeight          // allow browser window changes to update the canvas size
 
+    // ..: draw the element to the canvas
     ctx.save()
     ctx.translate(0, -car.y+canvas.height*0.7)    // 'camera' follows car over Y-axis
     road.draw(ctx)
+    for(let i=0; i<traffic.length; i++){
+        traffic[i].draw(ctx)
+    }
     car.draw(ctx)               // note: car will be an object with a 'draw' method that will take in 'ctx'
 
     ctx.restore()
