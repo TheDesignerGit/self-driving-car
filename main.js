77c98f451493501d0d281026f026fb22add4366d
JayJay Abrahams <jayson_lastong@yahoo.co.za>
2024-11-28 04:33:37 +0200

    implemented localStorage and genetic algorithm

--- main.js
+++ main.js
@@ -11,11 +11,41 @@ const road = new Road(carCanvas.width/2, carCanvas.width*0.9)
 // const car = new Car(road.getLaneCenter(1),100,30,50, "AI")        // 1st arg = which lane to start on
 const N = 100                                    //   number of cars to generate
 const cars = generateCars(N)
+const mutationRate = 0.1
+let bestCar = cars[0]
+if(localStorage.getItem('bestBrain')) {                 // load the previously saved bestCar
+    for(let i=0; i<cars.length; i++) {
+        cars[i].brain = JSON.parse(localStorage.getItem('bestBrain'))
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain, mutationRate)
+        }
+    }
+}
+
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(3), -300, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -900, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(3), -900, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -1100, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -1100, 30, 50, "DUMMY", 2)
 ]
 
 animate()
+
+
+function save() {                   // save the best progress
+    localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain))
+}
+
+function discardSave() {
+    localStorage.removeItem('bestBrain')
+}
 
 function generateCars(N){
     const cars = []
@@ -32,7 +62,7 @@ function animate (time) {
     for(let i=0; i<cars.length; i++){
         cars[i].update(road.borders, traffic)
     }
-    const bestCar = cars.find(              // select the car with lowest y-value (moved up the most)
+    bestCar = cars.find(              // select the car with lowest y-value (that's moved up the most)
         c => c.y == Math.min(
             ...cars.map(c => c.y)
         )
